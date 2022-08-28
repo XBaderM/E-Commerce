35638ab9f61292fa0515e5d98ec47bb22d48db5b
Bader Munir <bader.munir18@googlemail.com>
2022-08-28 11:40:37 +0100

    add get all products route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,6 +7,21 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [{
+        model: Category
+      },
+      {
+        model: Tag,
+        as: 'productTag'
+      }
+    ]
+  })
+  .then(dbProduct => res.json(dbProduct))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 // get one product
