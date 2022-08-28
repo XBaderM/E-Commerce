e965ac3f6aa779dee8d356d68de7a8ba84b487ec
Bader Munir <bader.munir18@googlemail.com>
2022-08-28 11:31:12 +0100

    add post update route with id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -58,6 +58,24 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbCategory => {
+    if (!dbCategory) {
+      res.status(404).json({
+        message: 'No category found with this id!'
+      });
+      return;
+    }
+    res.json(dbCategory);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
