302337eb721924a9f16c50323a28d23412533847
Bader Munir <bader.munir18@googlemail.com>
2022-08-28 12:10:08 +0100

    add delete route with id

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -132,6 +132,24 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbProduct => {
+    if (!dbProduct) {
+      res.status(404).json({
+        message: 'No product found with this id!'
+      });
+      return;
+    }
+    res.json(dbProduct);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
